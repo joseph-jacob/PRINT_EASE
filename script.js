efa16cb1d9e3ddbad42cbec5cb31f575227d039a
Johns Raju <83854933+JOHNSRAJU@users.noreply.github.com>
2023-05-21 16:36:05 +0000

    test

--- script.js
+++ script.js
@@ -66,3 +66,39 @@ function validatePassword(password1, password2) {
         return false; // Passwords do not match
     }
 }
+
+
+
+
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-analytics.js";
+import { getFirestore, addDoc, collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.12.1/firebase-firestore.js'
+const firebaseConfig = {
+  apiKey: "AIzaSyBT1jTxSYBFNwq9eFA7X5egl8NPaNeF1d0",
+  authDomain: "miniproject-599c9.firebaseapp.com",
+  projectId: "miniproject-599c9",
+  storageBucket: "miniproject-599c9.appspot.com",
+  messagingSenderId: "408288219519",
+  appId: "1:408288219519:web:0cef4bbae96354f30c4a4a",
+  measurementId: "G-5E0GSXFY3K"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+document.getElementById("signUpButton").onclick = async function (e) {
+    e.preventDefault()
+    const docRef = await addDoc(collection(db, "data"), {
+        Name: document.getElementById('name').value,
+        Email: document.getElementById('email').value
+    });
+    console.log("Document written with ID: ", docRef.id);
+    alert("Form submitted")
+    location.reload()
+};
+
+
+
+
+
+
