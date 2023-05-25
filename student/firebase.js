d4b6f0dcd30d8c726d2d67cd95fd1ba1d700c07d
Johns Raju <83854933+JOHNSRAJU@users.noreply.github.com>
2023-05-25 14:44:12 +0000

    5

--- student/firebase.js
+++ student/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js';
-import { getFirestore, collection, getDocs,where } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js';
+import { getFirestore, collection, getDocs,where ,query} from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js';
 import { getStorage, ref, getDownloadURL } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-storage.js';
 const firebaseConfig = {
     apiKey: "AIzaSyBCtvK5bUFqSRGWYTVXnNLHsSdSMMfyEdQ",
@@ -18,61 +18,59 @@ const auth = getAuth();
 const db = getFirestore();
 const storage = getStorage();
 
-auth.onAuthStateChanged(async function(user) {
-    if (user) {
-      console.log(user.uid)
-        // Retrieve data from Firestore
-        const collectionRef = collection(db, 'data');
-        const querySnapshot = await getDocs(collectionRef, where('uid', '==', user.uid));
-  
-        // Get the container element where the cards will be displayed
-        const cardContainer = document.getElementById('card-container');
-  
-        // Loop through the query snapshot and generate the HTML cards
-        querySnapshot.forEach(async (doc) => {
-          // Get the data from the document
-          const data = doc.data();
-  
-          // Create the card element
-          const card = document.createElement('div');
-          card.className = 'container';
-  
-          // Create the box element
-          const box = document.createElement('div');
-          box.className = 'box';
-  
-          // Create the card content
-          const cardContent = document.createElement('div');
-          cardContent.className = 'card-content';
-  
-          // Get the reference to the file in Firebase Storage
-          const storageRef = ref(storage, data.File);
-  
-          // Get the download URL for the file
-          const downloadURL = await getDownloadURL(storageRef);
-  
-          // Populate the card content with the retrieved data and download URL
-         
-          cardContent.innerHTML = `
-            <object data="${downloadURL}" type="application/pdf"></object>
-            <strong>${data.FName}</strong>
-            <span>Price :- </span><span>${data.Fprice}</span> <br>
-            <span>Date :- </span> <span>${data.Date}</span><br>
-          <a href=${downloadURL}><img src="../../images/open.gif" alt="Your GIF"></object></a>
+auth.onAuthStateChanged(async function (user) {
+  if (user) {
+    console.log(user.uid);
+    // Retrieve data from Firestore
+    const collectionRef = collection(db, 'data');
+    const querySnapshot = await getDocs(query(collectionRef, where('uid', '==', user.uid)));
 
-          `;
-  
-          // Append the card content to the box
-          box.appendChild(cardContent);
-  
-          // Append the box to the card
-          card.appendChild(box);
-  
-          // Append the card to the card container
-          cardContainer.appendChild(card);
-        });
-    } else {
-      // The user is not signed in.
-      window.location.href = "../.index.html";
-    }
+    // Get the container element where the cards will be displayed
+    const cardContainer = document.getElementById('card-container');
+
+    // Loop through the query snapshot and generate the HTML cards
+    querySnapshot.forEach(async (doc) => {
+      // Get the data from the document
+      const data = doc.data();
+
+      // Create the card element
+      const card = document.createElement('div');
+      card.className = 'container';
+
+      // Create the box element
+      const box = document.createElement('div');
+      box.className = 'box';
+
+      // Create the card content
+      const cardContent = document.createElement('div');
+      cardContent.className = 'card-content';
+
+      // Get the reference to the file in Firebase Storage
+      const storageRef = ref(storage, data.File);
+
+      // Get the download URL for the file
+      const downloadURL = await getDownloadURL(storageRef);
+
+      // Populate the card content with the retrieved data and download URL
+      cardContent.innerHTML = `
+        <object data="${downloadURL}" type="application/pdf"></object>
+        <strong>${data.FName}</strong>
+        <span>Price: ${data.Fprice}</span><br>
+        <span>Date: ${data.Date}</span><br>
+        <a href="${downloadURL}"><img src="../../images/open.gif" alt="Your GIF"></a>
+      `;
+
+      // Append the card content to the box
+      box.appendChild(cardContent);
+
+      // Append the box to the card
+      card.appendChild(box);
+
+      // Append the card to the card container
+      cardContainer.appendChild(card);
+    });
+  } else {
+    // The user is not signed in.
+    window.location.href = '../.index.html';
+  }
 });
