c27d59173052eebfedeb6a730e27e1d0207e6786
Johns Raju <83854933+JOHNSRAJU@users.noreply.github.com>
2023-06-02 02:36:11 +0000

    1

--- staff/update.js
+++ staff/update.js
@@ -26,4 +26,28 @@ var pdfDownloadURL = sessionStorage.getItem("pdfDownloadURL")
 const documentRef = doc(db, 'data', docId);
 await updateDoc(documentRef, { Fstatus: "Done" });
 console.log("set");
-window.location.href = "./email.html";
+
+var email = sessionStorage.getItem("email")
+var Fname = sessionStorage.getItem("Fname")
+var price = sessionStorage.getItem("price")
+
+var params = {
+    email: email,
+    Fname:Fname,
+    price:price,
+};
+
+const serviceID = "service_lgqfta2";
+const templateID = "template_cci5hjr";
+
+emailjs.send(serviceID, templateID, params).then(
+function (response) {
+  console.log("SUCCESS!", response.status, response.text);
+  window.location.href = pdfDownloadURL;
+},
+function (error) {
+  console.log("FAILED...", error);
+  alert("Error sending email: " + error);
+}
+);
+
