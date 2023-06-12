739b9a6b1f14460c515e26dcc4c59be2477de9ee
Johns Raju <johnsrajum@gmail.com>
2023-06-12 11:57:31 +0530

    Last

--- staff/firebase.js
+++ staff/firebase.js
@@ -1,5 +1,4 @@
 
-
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js';
 import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-analytics.js';
 import { getAuth, signOut } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js';
@@ -36,132 +35,138 @@ document.getElementById("search").addEventListener("click",function(event){
 });
 
 auth.onAuthStateChanged(async function (user) {
-  if (user.uid =='5w3QtqWjjSOazqmwX7teQEEPB6k2') {
-    const currentDate = new Date()
-    const year = currentDate.getFullYear();
-    const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Months are zero-based
-    const day = String(currentDate.getDate()).padStart(2, '0');
-    const date = `${year}-${month}-${day}`;
-    // Set the value of the date input field to the current date
-    document.getElementById('date').value = date;
-    // Function to handle the query when the date input value changes
-    const handleQuery = async function () {
-      // Clear the table body
-      const tableBody = document.getElementById('tbody');
-      tableBody.innerHTML = '';
-      const selectedDate = document.getElementById('date').value;
-      const selectedStatus = getSelectedRadioValue();
-    
-      console.log(selectedDate);
-      console.log(selectedStatus);
-    
-      const dataCollectionRef = collection(db, "data");
-      let querySnapshot;
-    
-      if (selectedStatus === 'all') {
-        querySnapshot = await getDocs(query(
-          dataCollectionRef,
-          orderBy("timestamp","desc"),
-          where('Date', '==', selectedDate)
-        ));
-      } else {
-        querySnapshot = await getDocs(query(
-          dataCollectionRef,
-          orderBy("timestamp"),
-          where('Date', '==', selectedDate),
-          where('Fstatus', '==', selectedStatus)
-        ));
-      }
-    
-      let count = 1;
-      for (const doc of querySnapshot.docs) {
-        const pdfRef = ref(storage, doc.data().File);
-        const audioRef = ref(storage, doc.data().Faudio);
-        const docId = doc.id;
-        // Access the 'Name' property from the document data
-        const pdfDownloadURL = await getDownloadURL(pdfRef);
-        const audioDownloadURL = await getDownloadURL(audioRef);
-        const row = document.getElementById("tbody").insertRow(0);
-        row.insertCell(0).innerHTML = count;
-        row.insertCell(1).innerHTML = `<a href="./othersProfile/index.html?uid=${doc.data().uid}&email=${doc.data().email}">${doc.data().email}</a>`;
-        row.insertCell(2).innerHTML = doc.data().FName;
-        row.insertCell(3).innerHTML = doc.data().Fprice;
-        row.insertCell(4).innerHTML = doc.data().Fmethod;
-        row.insertCell(5).innerHTML = doc.data().Fcount;
-        row.insertCell(6).innerHTML = doc.data().FType;
-        row.insertCell(7).innerHTML = doc.data().Fside;
-        row.insertCell(8).innerHTML = `<audio src="${audioDownloadURL}" controls></audio>`;
-        row.insertCell(9).innerHTML = `<img class="book" src="../images/open.gif" alt="Your GIF"></a>`;
-        if (doc.data().Fstatus == 'Done') {
-          row.insertCell(10).innerHTML = `<img class="tick" src="../images/tick.png" alt="Your GIF"></a>`;
-        } else if (doc.data().Fstatus == 'Processing') {
-          row.insertCell(10).innerHTML = `<input class="checbox" type="checkbox">`;
-          const checkElement = row.cells[10].querySelector(".checbox");
-          checkElement.onclick = async function () {
-            var params = {
-              email: doc.data().email,
-              Fname: doc.data().Fname,
-              price: doc.data().price,
-            };
-            const serviceID = "service_lgqfta2";
-            const templateID = "template_cci5hjr";
-
-            emailjs.send(serviceID, templateID, params).then(
-              function (response) {
-                console.log("SUCCESS!", response.status, response.text);
-                updatePending(docId, "Done")
-                row.insertCell(10).innerHTML = `<img class="tick" src="../images/tick.png" alt="Your GIF"></a>`;
-                checkElement.style.display="none"
-              },
-              function (error) {
-                console.log("FAILED...", error);
-                alert("Error sending email: " + error);
-              }
-            );
+  if (user) {
+    if(user.uid=="5w3QtqWjjSOazqmwX7teQEEPB6k2"){
+      const currentDate = new Date()
+      const year = currentDate.getFullYear();
+      const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+      const day = String(currentDate.getDate()).padStart(2, '0');
+      const date = `${year}-${month}-${day}`;
+      // Set the value of the date input field to the current date
+      document.getElementById('date').value = date;
+      // Function to handle the query when the date input value changes
+      const handleQuery = async function () {
+        // Clear the table body
+        const tableBody = document.getElementById('tbody');
+        tableBody.innerHTML = '';
+        const selectedDate = document.getElementById('date').value;
+        const selectedStatus = getSelectedRadioValue();
+      
+        console.log(selectedDate);
+        console.log(selectedStatus);
+      
+        const dataCollectionRef = collection(db, "data");
+        let querySnapshot;
+      
+        if (selectedStatus === 'all') {
+          querySnapshot = await getDocs(query(
+            dataCollectionRef,
+            orderBy("timestamp","desc"),
+            where('Date', '==', selectedDate)
+          ));
+        } else {
+          querySnapshot = await getDocs(query(
+            dataCollectionRef,
+            orderBy("timestamp"),
+            where('Date', '==', selectedDate),
+            where('Fstatus', '==', selectedStatus)
+          ));
+        }
+      
+        let count = 1;
+        for (const doc of querySnapshot.docs) {
+          const pdfRef = ref(storage, doc.data().File);
+          const audioRef = ref(storage, doc.data().Faudio);
+          const docId = doc.id;
+          // Access the 'Name' property from the document data
+          const pdfDownloadURL = await getDownloadURL(pdfRef);
+          const audioDownloadURL = await getDownloadURL(audioRef);
+          const row = document.getElementById("tbody").insertRow(0);
+          row.insertCell(0).innerHTML = count;
+          row.insertCell(1).innerHTML = `<a href="./othersProfile/index.html?uid=${doc.data().uid}&email=${doc.data().email}">${doc.data().email}</a>`;
+          row.insertCell(2).innerHTML = doc.data().FName;
+          row.insertCell(3).innerHTML = doc.data().Fprice;
+          row.insertCell(4).innerHTML = doc.data().Fmethod;
+          row.insertCell(5).innerHTML = doc.data().Fcount;
+          row.insertCell(6).innerHTML = doc.data().FType;
+          row.insertCell(7).innerHTML = doc.data().Fside;
+          row.insertCell(8).innerHTML = `<audio src="${audioDownloadURL}" controls></audio>`;
+          row.insertCell(9).innerHTML = `<img class="book" src="../images/open.gif" alt="Your GIF"></a>`;
+          if (doc.data().Fstatus == 'Done') {
+            row.insertCell(10).innerHTML = `<img class="tick" src="../images/tick.png" alt="Your GIF"></a>`;
+          } else if (doc.data().Fstatus == 'Processing') {
+            row.insertCell(10).innerHTML = `<input class="checbox" type="checkbox">`;
+            const checkElement = row.cells[10].querySelector(".checbox");
+            checkElement.onclick = async function () {
+              var params = {
+                email: doc.data().email,
+                Fname: doc.data().Fname,
+                price: doc.data().price,
+              };
+              const serviceID = "service_lgqfta2";
+              const templateID = "template_cci5hjr";
+  
+              emailjs.send(serviceID, templateID, params).then(
+                function (response) {
+                  console.log("SUCCESS!", response.status, response.text);
+                  updatePending(docId, "Done")
+                  row.insertCell(10).innerHTML = `<img class="tick" src="../images/tick.png" alt="Your GIF"></a>`;
+                  checkElement.style.display="none"
+                },
+                function (error) {
+                  console.log("FAILED...", error);
+                  alert("Error sending email: " + error);
+                }
+              );
+            }
+          }
+          count++;
+          var done = "Done";
+          const imageElement = row.cells[9].querySelector(".book");
+          imageElement.onclick = async function () {
+            updatePending(docId, "Processing")
+            window.open(pdfDownloadURL);
           }
         }
-        count++;
-        var done = "Done";
-        const imageElement = row.cells[9].querySelector(".book");
-        imageElement.onclick = async function () {
-          updatePending(docId, "Processing")
-          window.open(pdfDownloadURL);
-        }
-      }
-    };
-
-    // Add event listener to the date input field
-    document.getElementById("date").addEventListener("input", handleQuery);
-
-    // Call the handleQuery function initially to perform the query with the initial date value
-    handleQuery();
-
-    document.getElementById("logout").addEventListener("click", function (event) {
-      event.preventDefault();
-      signOut(auth).then(() => {
-        // Sign-out successful.
-        console.log('Sign-out successful.');
-        alert('Sign-out successful.');
-
-      }).catch((error) => {
-        // An error happened.
-        console.log('An error happened.');
+      };
+  
+      // Add event listener to the date input field
+      document.getElementById("date").addEventListener("input", handleQuery);
+  
+      // Call the handleQuery function initially to perform the query with the initial date value
+      handleQuery();
+  
+      document.getElementById("logout").addEventListener("click", function (event) {
+        event.preventDefault();
+        signOut(auth).then(() => {
+          // Sign-out successful.
+          console.log('Sign-out successful.');
+          alert('Sign-out successful.');
+  
+        }).catch((error) => {
+          // An error happened.
+          console.log('An error happened.');
+        });
       });
+  
+  
+      async function updatePending(docId, status) {
+        const documentRef = doc(db, 'data', docId);
+        await updateDoc(documentRef, { Fstatus: status });
+      }
+      var radios = document.getElementsByName('radio');
+  for (var i = 0; i < radios.length; i++) {
+    radios[i].addEventListener('change', function() {
+      var selectedValue = getSelectedRadioValue();
+      // Do something with the selected value
+      handleQuery();
     });
-
-
-    async function updatePending(docId, status) {
-      const documentRef = doc(db, 'data', docId);
-      await updateDoc(documentRef, { Fstatus: status });
+  }
+  
+    }else{
+      window.location.href = "../index.html";
     }
-    var radios = document.getElementsByName('radio');
-for (var i = 0; i < radios.length; i++) {
-  radios[i].addEventListener('change', function() {
-    var selectedValue = getSelectedRadioValue();
-    // Do something with the selected value
-    handleQuery();
-  });
-}
+    
 
   } else {
     window.location.href = "../index.html";
